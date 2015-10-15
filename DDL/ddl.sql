837fd4275618e32b83b31858a1a76f75a26bfb2e
gelton cruz <gelton@midiaplural.com.br>
2015-10-15 10:47:30 -0300

    CRIAR ENTIDADE ENTIDADE

--- DDL/ddl.sql
+++ DDL/ddl.sql
@@ -2,3 +2,9 @@
 mysql -u <user> - p <senha>;
 #CRIAR BANCO DE dados
 CREATE DATABASE <nomeDaBase>;
+#CRIAR ENTIDADE ENTIDADE
+CREATE TABLE <nomeDaTabela>(
+  matricula int(4) zerofill unsigned not null auto_increment,
+  nome varchar(60) not null,
+  primary key(matricula)
+)
