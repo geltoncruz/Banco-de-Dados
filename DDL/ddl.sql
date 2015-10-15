cad5c3b829509640c867debf794d3343e486aab6
gelton cruz <gelton@midiaplural.com.br>
2015-10-15 10:53:31 -0300

    Selecionar Banco

--- DDL/ddl.sql
+++ DDL/ddl.sql
@@ -2,6 +2,8 @@
 mysql -u <user> - p <senha>;
 #CRIAR BANCO DE dados
 CREATE DATABASE <nomeDaBase>;
+#Selecionar base de dados
+use <nomeDaBase>;
 #CRIAR ENTIDADE ENTIDADE
 CREATE TABLE <nomeDaTabela>(
   matricula int(4) zerofill unsigned not null auto_increment,
