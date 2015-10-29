6c4424b8b7239f755543532e12a8ff7d04c37ebb
gelton cruz <gelton@midiaplural.com.br>
2015-10-29 10:57:53 -0300

    Entidade funcionario

--- DDL/empregado.sql
+++ DDL/empregado.sql
@@ -11,7 +11,18 @@ create table funcao(
  descricao text,
  #Definir chave primária
  primary key(codigo)
- )
+ );
  #VERIFCAR TABELA CRIADA
  show tables;
+ #DESCRIÇÃO DA TABELA
+ describe funcao;
  
+ #CRIAR ENTIDADE FUNCIONÁRIO
+ create table funcionario(
+  matricula int(4) unsigned zerofill not null auto_increment,
+  nome varchar(100) not null,
+  codCargo int(4) unsigned zerofill not null,
+  #Definir Chaves
+  primary key(matricula),
+  constraint fk_funcionario_Cargo foreign key(codCargo) references funcao(codigo)
+  );
